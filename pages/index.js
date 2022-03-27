fcf43b2d6cda462b105e40ea4c66c998718ebd5f
Bahman World <bahman.world@gmail.com>
2022-03-27 21:39:13 +0430

    #3 Commit

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 export const getServerSideProps = async () => {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const json = await response.json();
-  const characters = json.results
+  const characters = json.results;
   return {
-    props: { characters }
+    props: { characters },
   };
 };
 
@@ -23,18 +23,24 @@ const Home = ({ characters }) => {
       </Head>
       <main className="container">
         <div>
-        <h1 style={{margin: 0, padding: 0}}>List of Characters:</h1>
-        <small style={{marginBottom: 20}}>Click on every item to show details.</small>
-        <ul style={{minWidth: 500}}>
-          {characters.map((character) => {
-            return <li>
-              <Link href={`/character/${character.id}`}>
-              {character.name}
-              </Link>
-              </li>;
-          })}
-        </ul>
-        <small style={{opacity: 0.5}}>Designed by Bahman World (bahman.world@gmail.com)</small>
+          <h1 style={{ margin: 0, padding: 0 }}>List of Characters:</h1>
+          <small style={{ marginBottom: 20 }}>
+            Click on every item to show details.
+          </small>
+          <ul style={{ minWidth: 500 }}>
+            {characters.map((character) => {
+              return (
+                <li key={character.id}>
+                  <Link href={`/character/${character.id}`}>
+                    {character.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <small style={{ opacity: 0.5 }}>
+            Designed by Bahman World (bahman.world@gmail.com)
+          </small>
         </div>
       </main>
     </div>
